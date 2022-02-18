5a44853dc2f72b113eb7b4b8ac2674af07d74a0b
Vivian Gomez <viviangomez0@gmail.com>
2022-02-17 22:31:50 -0500

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+  return(
+    <div>
+      <button 
+      onBlur={()=> console.log('Hey! Eyes on me!')} 
+      onFocus={()=> console.log('Good!')}
+      >Eyes on me</button>
+    </div>
+  ) 
+}
+
+export default EyesOnMe;// Code EyesOnMe Component Here
